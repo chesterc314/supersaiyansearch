5a2d7a7c941ae2c12d53d16e50e8b19383665307
chesterc314 <unset>
2020-08-28 21:30:02 +0200

    Added Clicks store site

---
+++ super-saiyan-search/Integration.Tests/ClicksStoreSiteUnitTest.cs
@@ -0,0 +1,21 @@
+using SuperSaiyanSearch.Domain.Interfaces;
+using Xunit;
+
+namespace SuperSaiyanSearch.Integration.Tests
+{
+    public class ClicksStoreSiteUnitTest
+    {
+        private readonly IStoreSite _clicksStoreSite;
+
+        public ClicksStoreSiteUnitTest()
+        {
+           _clicksStoreSite = new ClicksStoreSite(new WebScrapper());
+        }
+        [Fact]
+        public void GivenUrlWhenSearchingScrapThenReturnSiteContent()
+        {
+            var results = _clicksStoreSite.Search("bread");
+            Assert.NotNull(results);
+        }
+    }
+}
