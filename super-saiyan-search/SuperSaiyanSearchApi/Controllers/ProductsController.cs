b507782d73c0b060256b01cf950648c10d07877b
chesterc314 <unset>
2020-08-29 13:00:56 +0200

    Added error handling

--- super-saiyan-search/SuperSaiyanSearchApi/Controllers/ProductsController.cs
+++ super-saiyan-search/SuperSaiyanSearchApi/Controllers/ProductsController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SuperSaiyanSearch.Api;
 using SuperSaiyanSearch.Api.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace SuperSaiyanSearchApi.Controllers
 {
@@ -17,9 +20,27 @@ namespace SuperSaiyanSearchApi.Controllers
             _productApi = productApi;
         }
         [HttpGet]
-        public ProductsReadDto Get([FromQuery] string q)
+        public ActionResult<ProductsReadDto> Get([FromQuery] string q)
         {
-            return _productApi.Search(q);
+            var results = new ProductsReadDto(new List<ProductReadDto>());
+            try
+            {
+                results = _productApi.Search(q);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(new EventId(results.GetHashCode(), Guid.NewGuid().ToString()), ex, "Error occurred proccessing the request");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (results.TotalResults == 0)
+            {
+                var notFoundMessage = $"No products found for your search query: {q}";
+                _logger.LogInformation(notFoundMessage);
+                return NotFound(notFoundMessage);
+            }
+            _logger.LogInformation($"Total results returned: {results.TotalResults} for search query: {q}");
+            return Ok(results);
         }
     }
 }
