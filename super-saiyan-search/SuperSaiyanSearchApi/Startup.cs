433f93d88b64990733f923a18b7e4002bc27d1b0
chesterc314 <unset>
2020-09-04 22:21:32 +0200

    reverted to old domain for the time being

--- super-saiyan-search/SuperSaiyanSearchApi/Startup.cs
+++ super-saiyan-search/SuperSaiyanSearchApi/Startup.cs
@@ -34,7 +34,7 @@ namespace SuperSaiyanSearchApi
             services.AddMemoryCache();
             services.AddCors(c =>
             {
-                c.AddPolicy("AllowOrigin", options => options.WithOrigins("https://supersaiyansearch.co.za"));
+                c.AddPolicy("AllowOrigin", options => options.WithOrigins("https://chesterc314.github.io"));
             });
             services.AddControllers().AddNewtonsoftJson(options => options.UseMemberCasing())
             .AddJsonOptions(ops => ops.JsonSerializerOptions.IgnoreNullValues = true);
@@ -52,7 +52,7 @@ namespace SuperSaiyanSearchApi
 
             app.UseRouting();
 
-            app.UseCors(options => options.WithOrigins("https://supersaiyansearch.co.za"));
+            app.UseCors(options => options.WithOrigins("https://chesterc314.github.io"));
 
             app.UseAuthorization();
 
